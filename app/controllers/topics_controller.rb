3fdcb24cc26a29e364a99d6363a2244d412b289c
F <fwrizalman@gmail.com>
2016-08-04 22:53:30 +0800

    Complete topics crud

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,50 @@
+class TopicsController < ApplicationController
+  def index
+    @topics = Topic.all
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+    if @topic.save
+      redirect_to topic_path(@topic)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @topic = Topic.find(params[:id])
+  end
+
+  def edit
+    @topic = Topic.find(params[:id])
+  end
+
+  def update
+    @topic = Topic.find(params[:id])
+    if @topic.update(topic_params)
+      redirect_to topic_path(@topic)
+    else
+      redirect_to edit_topic_path(@topic)
+    end
+  end
+
+  def destroy
+    @topic = Topic.find_by(id: params[:id])
+    if @topic.destroy
+      redirect_to topics_path
+    else
+      redirect_to topic_path(@topic)
+    end
+  end
+
+  private
+
+    def topic_params
+      params.require(:topic).permit(:title, :description)
+    end
+end
