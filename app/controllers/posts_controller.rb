ab5af18b9b430717d96c417bd23458387f4e26d1
F <fwrizalman@gmail.com>
2016-08-05 12:02:27 +0800

    Nested posts under topics

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,44 +1,47 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all.order('created_at desc')
+    @topic = Topic.includes(:posts).find_by(id: params[:topic_id])
+    @posts = @topic.posts.order("created_at DESC")
   end
 
   def new
+    @topic = Topic.find_by(id: params[:topic_id])
     @post = Post.new
   end
 
   def create
-    @post = Post.new(post_params)
+    @topic = Topic.find_by(id: params[:topic_id])
+    @post = Post.new(post_params.merge(topic_id: params[:topic_id]))
+
     if @post.save
-      redirect_to post_path(@post)
+      redirect_to topic_posts_path(@topic)
     else
-      render :new
+      redirect_to new_topic_post_path(@topic)
     end
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
-
   def edit
-    @post = Post.find(params[:id])
+    @post = Post.find_by(id: params[:id])
+    @topic = @post.topic
   end
 
   def update
-    @post = Post.find(params[:id])
-    if @post.update(Post_params)
-      redirect_to post_path(@post)
+    @topic = Topic.find_by(id: params[:topic_id])
+    @post = Post.find_by(id: params[:id])
+
+    if @post.update(post_params)
+      redirect_to topic_posts_path(@topic)
     else
-      redirect_to edit_post_path(@post)
+      redirect_to edit_topic_post_path(@topic, @post)
     end
   end
 
   def destroy
     @post = Post.find_by(id: params[:id])
+    @topic = @post.topic
+
     if @post.destroy
-      redirect_to posts_path
-    else
-      redirect_to post_path(@post)
+      redirect_to topic_posts_path(@topic)
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'landing#index'
-  resources :topics
-  resources :posts
+  resources :topics, except: [:show] do
+    resources :posts, except: [:show]
+  end
 end
