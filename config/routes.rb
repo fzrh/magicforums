fad9427a5d49942220956a82e4701f34e341f4f9
F <fwrizalman@gmail.com>
2016-08-08 21:16:31 +0800

    Change root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'landing#index'
+  root 'topics#index'
   resources :topics, except: [:show] do
     resources :posts, except: [:show] do
       resources :comments, except: [:show]
