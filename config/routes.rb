cf76bd22cfc3662620aeae3022f93b0ac6b0c472
F <fwrizalman@gmail.com>
2016-08-10 23:42:41 +0800

    Add password resets

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   end
   resources :users, except: [:index, :show, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
+  resources :password_resets, only: [:new, :create, :edit, :destroy]
 end
